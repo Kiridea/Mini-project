21bef4bcf6bb1b5b9ce051860f48704517dba925
Esteban Escalante <escalanteaveleyra@gmail.com>
2024-07-23 17:08:24 +0200

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,16 @@
+import GithubImage from '../assets/github.svg'
 import './footer.css';
 
 function Footer() {
     return (
-<<<<<<< HEAD
         <div className='footer-container'>  
             <hr/>     
-             <a className='link' href="https://github.com/Kiridea/Mini-project">
-             <img className="github" src="./assets/github.svg" alt="" /> 
+             <a href="https://github.com/Kiridea/Mini-project">
+             <img className="github" src={GithubImage} alt="Github" /> 
              </a>
         </div>
 
     );
-=======
-        < >
-        </>
-    )
->>>>>>> 6d940aafab543635d34e42e3835a22c5993e5f4d
 }
 
 export default Footer
