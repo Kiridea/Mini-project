31f13e20f74262489ad72eddd984241fb6ffb1c5
Kim Delgado <kimdelgado1570@gmail.com>
2024-07-26 17:10:23 +0200

    create add form

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,22 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import AddForm from './components/AddForm'
+// import UpdateApartment from './pages/UpdateApartment'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <AddForm />
       <div id="main">
       <Sidebar />
       <Routes>
         <Route path="/ApartmentDetailsPage/:apartment_id" element={<AparmentDetailPage/>}></Route>
         <Route path="/" element={<ApartmentList />}/>
         <Route path="/about" element={<About />}/>
+        {/* <Route path="/edit-form" element={<UpdateApartment />} /> */}
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
       </div>
---
+++ src/components/AddForm.jsx
@@ -0,0 +1,53 @@
+import './AddForm.css'
+
+function AddForm() {
+
+
+    return(
+        <form>
+            <h3>Add an Apartment</h3>
+            <div className="inputs">
+                <label className="text-inputs">
+                    Location
+                    <input type="text" name="location" placeholder="Apartment location"/>
+                </label>
+                <label className="text-inputs">
+                    Neighbourhood
+                    <input type="text" name="neighbourhood" placeholder="Apartment neighbourhood"/>
+                </label>
+                <label className="text-inputs">
+                    Name
+                    <input type="text" name="apartmentName" placeholder="Add a listing name" />
+                </label>
+                <label className="text-inputs">
+                    Description
+                    <input type="text" name="description" placeholder="Apartment Description"/>
+                </label>
+                <label className="text-inputs">
+                    Image
+                    <input type="text" name="image" placeholder="Image url"/>
+                </label>
+                <label className="num-inputs">
+                    Accommodates
+                    <input type="number" name="accommodates"/>
+                </label>
+                <label className="num-inputs">
+                    Bedrooms
+                    <input type="number" name="bedrooms"/>
+                </label>
+                <label className="num-inputs">
+                    Beds
+                    <input type="number" name="beds"/>
+                </label>
+                <label className="num-inputs">
+                    Price
+                    <input type="number" name="price"/>€
+                </label>
+
+            </div>
+            <button type="submit">Add apartment</button>
+        </form>
+    )
+}
+
+export default AddForm;
